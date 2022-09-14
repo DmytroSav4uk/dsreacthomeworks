1a3a0b8bdf9691d365246c320f88ccce3f321ec3
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-09-14 13:46:13 +0300

    completed homework

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import Simpson from "./simpsons/simpson";
-import  "./App.css";
-import family from "./simpsons/family"
+import Simpson from "./Characters/simpson";
+import "./App.css";
+import Characters from "./Characters/rickmorty";
+
 
 
 function App() {
+
     return (
-        <button onClick={family}>click to see Simpson family</button>
-    );
-}
+
+        <div className='main'>
+            <div className='simpsons'>
+                <Simpson name={'Bart'} pic={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
+                <Simpson name={'Homer'} pic={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
+                <Simpson name={'Marge'} pic={'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}/>
+                <Simpson name={'Lisa'} pic={'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}/>
+                <Simpson name={'Maggie'} pic={'https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png'}/>
+            </div>
+
+                <Characters/>
+
+         </div>
+            )}
+
+
 
 export default App;
-// <div className='wrap'>
-//     <Simpson name={'Bart'} pic={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
-//     <Simpson name={'Homer'} pic={'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
-//     <Simpson name={'Marge'} pic={'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}/>
-//     <Simpson name={'Lisa'} pic={'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}/>
-//     <Simpson name={'Maggie'} pic={'https://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png'}/>
-// </div>
---
+++ src/Characters/char.js
@@ -0,0 +1,16 @@
+const Char = (props) => {
+    let {id ,name,status,species,gender,image} = props;
+    return (
+        <div className='char'>
+            <img src={image}   alt={'image'}/>
+
+            <div className='info'>
+            <h1>{id} - {name}</h1>
+            <h2>{status}</h2>
+            <p>{species} - {gender}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Char;
