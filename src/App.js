33b9d961cd0901b5ec97f6655c01bdd8cff503b7
DmytroSav4uk <dmytro.savchuk.ua@gmail.com>
2022-09-18 14:33:08 +0300

    completed 1'st task of 2'nd homework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Users from "./Users/Users";
  import "./styles/homework2.css"
 import ChooseUser from "./Services/ChooseUser";
+import Simpson from "./Characters/Simpson";
+import Rickmorty from "./Characters/Rickmorty";
 
 function App() {
     return (
 <div>
-
     <Users/>
             <ChooseUser/>
 </div>
